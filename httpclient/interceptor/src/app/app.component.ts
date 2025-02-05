3b0c07f88fc0e3caa305d820eecfa348cf3f5fea
MrKatsu <mrk4tsu@gmail.com>
2025-02-05 20:21:19 +0700

    HttpParams & Object Literals

--- httpclient/interceptor/src/app/app.component.ts
+++ httpclient/interceptor/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import {Component, inject} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
-import {HttpClient} from '@angular/common/http';
-import {CommandModule} from '@angular/cli/src/command-builder/command-module';
+import {HttpClient, HttpParams} from '@angular/common/http';
 import {CommonModule} from '@angular/common';
 
 @Component({
@@ -15,7 +14,27 @@ export class AppComponent {
   data: any
 
   getProduct() {
-    this.http.get('https://localhost:5000/api/productpublics/list').subscribe(response => {
+    let httpParams = new HttpParams();
+    /*Ví dụ: Có 1 API lấy danh sách sản phẩm kèm filter
+    api/productpublics/list?KeyWord=1&CategoryId=1&CategorySeoAlias=window&PageIndex=1&PageSize=10
+    Tới đoạn này ta đang có 1 API với không có tham số truyền vào
+     */
+    let param1 = httpParams.set('KeyWord', '1'); // Thêm tham số KeyWord=1
+    //Lúc này api sẽ là: api/productpublics/list?KeyWord=1
+    let param2 = param1.set('CategoryId', '1'); // Thêm tham số CategoryId=1
+    //lúc này api sẽ là: api/productpublics/list?KeyWord=1&CategoryId=1
+    let param3 = param2.set('CategorySeoAlias', 'window'); // Thêm tham số CategorySeoAlias=window
+    //lúc này api sẽ là: api/productpublics/list?KeyWord=1&CategoryId=1&CategorySeoAlias=window
+    let param4 = param3.set('PageIndex', '1'); // Thêm tham số PageIndex=1
+    //lúc này api sẽ là: api/productpublics/list?KeyWord=1&CategoryId=1&CategorySeoAlias=window&PageIndex=1
+    let param5 = param4.set('PageSize', '10'); // Thêm tham số PageSize=10
+    //lúc này api sẽ là: api/productpublics/list?KeyWord=1&CategoryId=1&CategorySeoAlias=window&PageIndex=1&PageSize=10
+    //Nếu muốn thêm tham số nữa thì tiếp tục thêm như trên
+    const params = {id: 10}
+    this.http.get('https://localhost:5000/api/productpublics/list',
+      {
+        params: param5
+      }).subscribe(response => {
       this.data = response;
     });
   }
